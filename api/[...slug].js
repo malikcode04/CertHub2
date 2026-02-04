295a9aef5fec316d8226103bb8961aeedeec453e
malik <malikajaz2894@gmail.com>
2026-02-05 01:34:19 +0530

    debug: Bare bones Express in api/[...slug].js

--- api/[...slug].js
+++ api/[...slug].js
@@ -1,3 +1,11 @@
-import app from '../server.js';
+import express from 'express';
+const app = express();
+
+app.get('/api/debug/test', (req, res) => {
+    res.json({
+        message: 'Express catch-all is working',
+        timestamp: new Date().toISOString()
+    });
+});
 
 export default app;
